0e24e7519b3d0de7441d5c08348798af5eff8abe
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-06-23 10:29:01 +0700

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import airbnbLogo from "../assets/airbnb-logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={airbnbLogo} alt="logo" className="nav-logo" />
+    </nav>
+  );
+}
