2aa999fc34f6e6b639b3f0a44ef4293a6cb9fcab
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-06-24 07:17:40 +0700

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,13 +7,15 @@ export default function Card() {
       <div className="card--stats">
         <img src={katrie} alt="images" className="card--image" />
         <div className="card--stats">
-          <img src={star} alt="images" />
+          <img src={star} alt="images" className="card--star" />
           <span>5.0</span>
           <span>(6)</span>
           <span>USA</span>
         </div>
         <p>Life Lessons with Katie Zafares</p>
-        <p>From $136 / person</p>
+        <p>
+          <span className="bold">From $136</span> / person
+        </p>
       </div>
     </div>
   );
